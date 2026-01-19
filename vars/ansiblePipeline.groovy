3d078e230c7f82b72aadc09a773829460a9fa54c
Tanishka Singla <tannusingla185@gmail.com>
2026-01-19 18:22:16 +0530

    Fix jenkinsfile

--- vars/ansiblePipeline.groovy
+++ vars/ansiblePipeline.groovy
@@ -1,59 +1,30 @@
-def call() {
+def call(Map overrideConfig = [:]) {
 
-    def config
+    def config = [:]
+
     script {
         config = readYaml(
             text: libraryResource('ansible/config.yaml')
         )
     }
 
-    pipeline {
-        agent any
+    // Allow Jenkinsfile to override config values
+    config << overrideConfig
 
-        stages {
+    stage('Ansible Execution') {
+        sh """
+        pwd
+        ls -la
+        ansible-playbook \
+          -i ${config.INVENTORY_FILE} \
+          ${config.ANSIBLE_PLAYBOOK}
+        """
+    }
 
-            stage('Clone') {
-                steps {
-                    git branch: config.GIT_BRANCH,
-                        url: config.GIT_REPO_URL
-                }
-            }
-
-            stage('User Approval') {
-                when {
-                    expression { config.KEEP_APPROVAL_STAGE }
-                }
-                steps {
-                    input message: "Approve deployment to ${config.ENVIRONMENT}?"
-                }
-            }
-
-            stage('Ansible Execution') {
-                steps {
-                    sh """
-			pwd
-			ls -la
-                    ansible-playbook \
-                      -i ${config.INVENTORY_FILE} \
-                      ${config.ANSIBLE_PLAYBOOK}
-                    """
-                }
-            }
-        }
-
-        post {
-            success {
-                slackSend(
-                    channel: config.SLACK_CHANNEL_NAME,
-                    message: "✅ ${config.ACTION_MESSAGE}"
-                )
-            }
-            failure {
-                slackSend(
-                    channel: config.SLACK_CHANNEL_NAME,
-                    message: "❌ ${config.ACTION_MESSAGE}"
-                )
-            }
-        }
+    if (config.SLACK_CHANNEL_NAME) {
+        slackSend(
+            channel: config.SLACK_CHANNEL_NAME,
+            message: "✅ ${config.ACTION_MESSAGE}"
+        )
     }
 }
